f0bcdf66c54747acf592253509fab6b7e7d4e84a
salvadomingo <salvadomingob@gmail.com>
2013-07-12 06:07:08 +0000

    modified

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile(index.html,encoding);
+  response.send(fs.readFile(index.html,encoding));
 });
 
 var port = process.env.PORT || 5000;
